ee6eb35a5b49b9bc4e00a29448e71a6f44bc7e35
ezethan97 <ezethan97@gmail.com>
2023-03-22 11:22:36 -0500

    Added navbar and working login and register pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutComponent,
+    ContactComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    RouterModule.forRoot([
+    {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent}
+  ]),],
   providers: [],
   bootstrap: [AppComponent]
 })
